65197c18084d58370eba75d7f0dc8808ecf69b9d
KevPF98 <kevinsk8.1998@gmail.com>
2024-11-11 04:49:15 -0300

    unifique excepciones de butacas, modique tipos de coleccion en gestorfuncion y butaca

---
+++ Gestion-Cine/src/Gestores/GestorButacas.java
@@ -0,0 +1,80 @@
+package Gestores;
+
+import Excepciones.ButacaEnEstadoActualException;
+import Excepciones.ButacaInexistenteException;
+import Modelos.Cine.Butaca;
+
+import java.util.TreeMap;
+
+public class GestorButacas {
+
+    //Atributos:
+
+    private TreeMap<Integer, Butaca> butacas;
+    // Map: la más eficiente para insertar, buscar y borrar.
+    // TreeMap para mantener el orden por numero de butaca.
+
+    //Constructor:
+
+    public GestorButacas(int totalButacas){
+        butacas = new TreeMap<>();
+        for(int i = 1; i <= totalButacas; i++){
+            butacas.put(i, new Butaca(i));
+        }
+    }
+
+    /* Métodos: */
+
+    //Reservar:
+
+    public void reservarButaca(int numButaca, String dniCliente) throws ButacaInexistenteException, ButacaEnEstadoActualException {
+
+        //Primero, comprueba que el numero no sea negativo.
+        if(numButaca < 0)
+            throw new ButacaInexistenteException("El número: " + numButaca +
+                    " no es válido. Éste no puede ser negativo.");
+
+        //Segundo, comprueba que el número no se pase de la colección.
+        if(!butacas.containsKey(numButaca))
+            throw new ButacaInexistenteException(numButaca);
+
+        Butaca butaca = butacas.get(numButaca);
+
+        //Tercero, verifica que la butaca no se encuentre reservada.
+        if(butaca.getDniCliente() != null)
+            throw new ButacaEnEstadoActualException("La butaca " + numButaca +
+                    " ya se encuentra reservada, por el cliente de DNI: " +
+                    butaca.getDniCliente() + ".");
+
+        //Si no lanza ninguna excepción, hacemos la reserva y notificamos:
+
+        butaca.setDniCliente(dniCliente);
+        System.out.println("Butaca " + numButaca + " reservada con éxito, " +
+                "para el cliente con DNI: " + butaca.getDniCliente() + ".");
+
+    }
+
+    //Cancelar reserva:
+
+    public void liberarButaca(int numButaca) throws ButacaInexistenteException, ButacaEnEstadoActualException{
+        if(!butacas.containsKey(numButaca)){
+            throw new ButacaInexistenteException(numButaca);
+        }
+        Butaca butaca = butacas.get(numButaca);
+        if(butaca.getDniCliente() == null){
+            throw new ButacaEnEstadoActualException("La butaca ya se encuentra libre.");
+        }
+        else{
+            butaca.setDniCliente(null);
+        }
+    }
+
+    //Imprimir el estado de todas las butacas:
+
+    public void imprimirButacas() {
+        for (Butaca butaca : butacas.values()) {
+            System.out.println(butaca);
+        }
+    }
+
+}
---
+++ Gestion-Cine/src/Modelos/Cine/Entrada.java
@@ -0,0 +1,99 @@
+package Modelos.Cine;
+
+import Enumeraciones.EstadoFuncion;
+import Enumeraciones.TipoTicket;
+import Excepciones.ButacaEnEstadoActualException;
+import Excepciones.ButacaInexistenteException;
+
+import java.util.Objects;
+
+public class Entrada {
+
+    //Atributos:
+
+    private int idBoleta;
+    public static int contador = 0;
+    private Funcion funcion;
+    private TipoTicket tipoTicket;
+    private EstadoFuncion estadoFuncion;
+
+    //Constructor:
+
+    public Entrada(Funcion funcion, TipoTicket tipoTicket, EstadoFuncion estadoFuncion) {
+        this.idBoleta = ++contador;
+        this.funcion = funcion;
+        this.tipoTicket = tipoTicket;
+        this.estadoFuncion = estadoFuncion;
+    }
+
+    //Métodos:
+
+    public void reservarButaca(int numero, String cliente) throws ButacaInexistenteException, ButacaEnEstadoActualException { //delegamos
+        funcion.getSala().getTotalButacas().reservarButaca(numero, cliente);
+        //manejaremos la excepción en menu.
+    }
+
+    public void liberarButaca(int numero) throws ButacaInexistenteException, ButacaEnEstadoActualException { //delegamos
+        funcion.getSala().getTotalButacas().liberarButaca(numero);
+        //manejaremos la excepción en menu.
+    }
+
+    //Getters y setters:
+
+    public int getIdBoleta() {
+        return idBoleta;
+    }
+
+    public Funcion getFuncion() {
+        return funcion;
+    }
+
+    public void setFuncion(Funcion funcion) {
+        this.funcion = funcion;
+    }
+
+    public TipoTicket getTipoTicket() {
+        return tipoTicket;
+    }
+
+    public void setTipoTicket(TipoTicket tipoTicket) {
+        this.tipoTicket = tipoTicket;
+    }
+
+    public EstadoFuncion getEstadoFuncion() {
+        return estadoFuncion;
+    }
+
+    public void setEstadoFuncion(EstadoFuncion estadoFuncion) {
+        this.estadoFuncion = estadoFuncion;
+    }
+
+    //Equals y HashCode:
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        Entrada entrada = (Entrada) object;
+        return idBoleta == entrada.idBoleta;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(idBoleta);
+    }
+
+    //toString:
+
+    @Override
+    public String toString() {
+        return  "\n-----------------\n" +
+                "Id de la boleta: " + idBoleta + ".\n" +
+                "Función: " + funcion + ".\n" +
+                "Tipo de entrada: " + tipoTicket + ".\n" +
+                "Estado de la función: " + estadoFuncion +
+                ".\n" +
+                "\n-----------------\n";
+    }
+
+}
