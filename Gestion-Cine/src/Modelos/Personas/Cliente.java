bcae3139e2297bf8b916dfd1dd7373771a9ca6db
Rocio <rocioromanazzi@gmail.com>
2024-11-16 22:14:11 -0300

    Agrego metodos de eliminacion de tarjetas y direcciones a clientes

--- Gestion-Cine/src/Modelos/Personas/Cliente.java
+++ Gestion-Cine/src/Modelos/Personas/Cliente.java
@@ -8,48 +8,56 @@ import java.util.ArrayList;
 
 public class Cliente extends Persona implements IVisualizable {
 
-     private ArrayList<TarjetaBanco> tarjetasRegistradas;
-     private ArrayList<Direccion> direcciones;
+    private ArrayList<TarjetaBanco> tarjetasRegistradas;
+    private ArrayList<Direccion> direcciones;
 
-     public Cliente(String nombre, String apellido, String dni, String email, LocalDate fechaNacimiento) {
-          super(nombre, apellido, dni, email, fechaNacimiento);
-     }
+    public Cliente(String nombre, String apellido, String dni, String email, LocalDate fechaNacimiento) {
+        super(nombre, apellido, dni, email, fechaNacimiento);
+    }
 
-     public ArrayList<TarjetaBanco> getTarjetasRegistradas() {
-          return tarjetasRegistradas;
-     }
+    public ArrayList<TarjetaBanco> getTarjetasRegistradas() {
+        return tarjetasRegistradas;
+    }
 
-     public ArrayList<Direccion> getDirecciones() {
-          return direcciones;
-     }
+    public ArrayList<Direccion> getDirecciones() {
+        return direcciones;
+    }
 
-     public void agregarTarjeta(TarjetaBanco tarjetaBanco) {
-          this.tarjetasRegistradas.add(tarjetaBanco);
-     }
+    public void agregarTarjeta(TarjetaBanco tarjetaBanco) {
+        this.tarjetasRegistradas.add(tarjetaBanco);
+    }
 
-     public void agregarDireccion(Direccion direccion) {
-          this.direcciones.add(direccion);
-     }
+    public void eliminarTarjeta(TarjetaBanco tarjetaBanco) {
+        this.tarjetasRegistradas.remove(tarjetaBanco);
+    }
 
-     @Override
-     public String toString() {
-          return super.toString() +
-                  "direcciones=" + direcciones +
-                  '}';
-     }
+    public void agregarDireccion(Direccion direccion) {
+        this.direcciones.add(direccion);
+    }
 
-     @Override
-     public void verCartelera() {
+    public void eliminarDireccion(Direccion direccion) {
+        this.direcciones.remove(direccion);
+    }
 
-     }
+    @Override
+    public String toString() {
+        return super.toString() +
+                "direcciones=" + direcciones +
+                '}';
+    }
 
-     @Override
-     public void verReservas() {
+    @Override
+    public void verCartelera() {
 
-     }
+    }
 
-     @Override
-     public void verFunciones() {
+    @Override
+    public void verReservas() {
 
-     }
+    }
+
+    @Override
+    public void verFunciones() {
+
+    }
 }
